7f7212263671d247f0b94cc76b05a2272cb405e7
Sanky2024 <48628985+Sanky2024@users.noreply.github.com>
2023-01-20 12:14:58 +0530

    Update staticCode.groovy

--- vars/staticCode.groovy
+++ vars/staticCode.groovy
@@ -1,5 +1,20 @@
 def call(){
-  withSonarQubeEnv(credentialsId: 'sonar-api') {
-     sh 'mvn clean package sonar:sonar'
+  environment
+            {
+                scannerHome = tool "${SONARSCANNER}"
+            }
+            steps
+            {
+               withSonarQubeEnv("${SONARSERVER}")
+               {
+                   sh '''${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=vprofile \
+                   -Dsonar.projectName=vprofile \
+                   -Dsonar.projectVersion=1.0 \
+                   -Dsonar.sources=src/ \
+                   -Dsonar.java.binaries=target/test-classes/com/visualpathit/account/controllerTest/ \
+                   -Dsonar.junit.reportsPath=target/surefire-reports/ \
+                   -Dsonar.jacoco.reportsPath=target/jacoco.exec \
+                   -Dsonar.java.checkstyle.reportPaths=target/checkstyle-result.xml'''
+              }
   }
 } 
